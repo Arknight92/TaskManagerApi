f89de129f00265df2dbdc96ca3ecaeedcae89b9f
Arknight92 <138229670+Arknight92@users.noreply.github.com>
2026-02-02 15:41:45 +0100

    Removed template files and start basic get and post methods
    - One method using POST to create the basics task data
    - One method using GET to check the existing tasks

---
+++ TaskManagerApi/Controllers/TaskController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagerApi.Models;
+
+namespace TaskManagerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private static List<TaskItem> _tasks = new List<TaskItem>();
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<TaskItem>> GetAll()
+        {
+            return Ok(_tasks);
+        }
+
+        [HttpPost]
+        public ActionResult<TaskItem> Create(TaskItem task)
+        {
+            task.Id = _tasks.Count + 1;
+            task.CreateAt = DateTime.UtcNow;
+            _tasks.Add(task);
+
+            return CreatedAtAction(nameof(GetAll), new { id = task.Id }, task);
+        }
+    }
+}
---
+++ TaskManagerApi/Models/TaskItem.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskManagerApi.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public bool IsCompleted {  get; set; }
+        public DateTime CreateAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
