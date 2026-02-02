1a7d847cbb38b807ef9803301a921a2e235f805e
Arknight92 <138229670+Arknight92@users.noreply.github.com>
2026-02-02 16:18:54 +0100

    Change system to handle data in database instead of local list
    - Data saved in database
    - Can get data after closing and starting app again

--- TaskManagerApi/Controllers/TaskController.cs
+++ TaskManagerApi/Controllers/TaskController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using TaskManagerApi.Data;
 using TaskManagerApi.Models;
 
 namespace TaskManagerApi.Controllers
@@ -8,21 +11,30 @@ namespace TaskManagerApi.Controllers
     [ApiController]
     public class TaskController : ControllerBase
     {
-        private static List<TaskItem> _tasks = new List<TaskItem>();
+        // private static List<TaskItem> _tasks = new List<TaskItem>();
+        private readonly AppDbContext _context;
 
+        public TaskController(AppDbContext context)
+        {
+            _context = context;
+        }
 
         [HttpGet]
-        public ActionResult<IEnumerable<TaskItem>> GetAll()
+        public async Task<ActionResult<IEnumerable<TaskItem>>> GetAll()
         {
-            return Ok(_tasks);
+            var tasks = await _context.Tasks.ToListAsync();
+            return Ok(tasks);
         }
 
         [HttpPost]
-        public ActionResult<TaskItem> Create(TaskItem task)
+        public async Task<ActionResult<TaskItem>> Create(TaskItem task)
         {
-            task.Id = _tasks.Count + 1;
             task.CreateAt = DateTime.UtcNow;
-            _tasks.Add(task);
+
+            _context.Tasks.Add(task);
+
+            // change object into SQL, sennd it to database and get generated ID
+            await _context.SaveChangesAsync();
 
             return CreatedAtAction(nameof(GetAll), new { id = task.Id }, task);
         }
--- TaskManagerApi/Program.cs
+++ TaskManagerApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagerApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// adding EntityFrameworkCore into dependency injection system
+// Configure SQLite and create tasks.db file
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data source=tasks.db"));
 
 var app = builder.Build();
 
